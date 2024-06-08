cbec6f473aa0c2040fc4a328816c397094960816
anderwolfe <anderwolfe@gmail.com>
2024-06-08 09:47:03 -0500

    update

--- generate_index.js
+++ generate_index.js
@@ -3,39 +3,54 @@ const path = require('path');
 
 const directoryPath = path.join(__dirname, 'watchlist');
 
-fs.readdir(directoryPath, (err, files) => {
-  if (err) {
-    return console.error('Unable to scan directory: ' + err);
-  }
+function readDirectory(dirPath, folderName = '') {
+  let fileList = [];
+  const files = fs.readdirSync(dirPath);
 
-  let fileList = files
-    .filter(file => path.extname(file) === '.url')
-    .map(file => {
-      const filePath = path.join(directoryPath, file);
+  files.forEach(file => {
+    const filePath = path.join(dirPath, file);
+    const stat = fs.statSync(filePath);
+
+    if (stat.isDirectory()) {
+      const subfolderContent = readDirectory(filePath, path.join(folderName, file));
+      if (subfolderContent.length > 0) {
+        fileList.push(`
+          <h2>${path.join(folderName, file)}</h2>
+          <hr>
+          <ul>
+            ${subfolderContent.join('\n')}
+          </ul>
+          <p style="page-break-after: always;"></p>
+        `);
+      }
+    } else if (path.extname(file) === '.url') {
       const content = fs.readFileSync(filePath, 'utf-8');
       const urlMatch = content.match(/URL=(.*)/);
       const url = urlMatch ? urlMatch[1] : '#';
-      return `<li><a href="${url}">${file}</a></li>`;
-    })
-    .join('\n');
+      const displayName = folderName ? path.join(folderName, file) : file;
+      fileList.push(`<li><a href="${url}">${displayName}</a></li>`);
+    }
+  });
 
-  let htmlContent = `
-  <!DOCTYPE html>
-  <html lang="en">
-  <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Directory Listing</title>
-  </head>
-  <body>
-    <h1>Watchlist</h1>
-    <ul>
-      ${fileList}
-    </ul>
-  </body>
-  </html>
-  `;
+  return fileList;
+}
 
-  fs.writeFileSync(path.join(__dirname, 'watchlist.html'), htmlContent, 'utf8');
-  console.log('watchlist.html has been generated');
-});
+const fileList = readDirectory(directoryPath).join('\n');
+
+const htmlContent = `
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Directory Listing</title>
+</head>
+<body>
+  <h1>Watchlist</h1>
+  ${fileList}
+</body>
+</html>
+`;
+
+fs.writeFileSync(path.join(__dirname, 'watchlist.html'), htmlContent, 'utf8');
+console.log('watchlist.html has been generated');
